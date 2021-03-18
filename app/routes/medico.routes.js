ca243b717a460a0f04d8dcec39e28a6ad07c3e0a
pebueno <pedroivobu@gmail.com>
2021-03-18 16:10:20 -0300

    Retirando pesquisa por CEP

--- app/routes/medico.routes.js
+++ app/routes/medico.routes.js
@@ -1,24 +1,15 @@
 const medicos = require("../controllers/medico.controller.js");
-const getCepInfo = require("../controllers/cep.controller.js");
-const { correiosBrasil } = require("correios-brasil");
 
 module.exports = app => {
 //POST
 app.post("/post-medico-details",medicos.postmedicodetails);
-app.post('/', (req, res) => {
-    correios.consultarCep({ cep: res.body.cep }, (err, result) => {
-        return res.json(result)
-    })
-})
 
 //FIND
 app.post("/get-medico-details",medicos.getmedicodetails);
-app.post("/:cep", getCepInfo.getCepInfo);
 
 //UPDATE
- app.post("/update-details",medicos.updatedetails);
+ app.post("/update-details", medicos.updatedetails);
 
 //DELETE
-app.post("/delete-details",medicos.deletedetails);
-
+app.post("/delete-details", medicos.deletedetails);
 };
--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 require("./app/routes/medico.routes.js")(app);
-
 const PORT = process.env.PORT || 7000;
-
 app.listen(PORT, () => {
 console.log(`Server is running on port ${PORT}.`);
 })
